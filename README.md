f3d01314e3b07cd5da61857f741570e0bc912935
zhangpengfei <zhangpengfei@duiba.com.cn>
2018-11-22 12:05:16 +0800

    update

---
+++ README.md
@@ -0,0 +1,18 @@
+# React UI实现经典框架 你值得拥有！
+
+##  技术栈
+react(16.2.0) + webpack(4.3) + redux + react-router + antd(3.3.3)
+
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat)](LICENSE)
+
+## 运行
+
+```bash
+
+ npm install --registry  https://registry.npm.taobao.org 
+ npm start
+ 
+```
+
+## 说明
+
---
+++ webpack.dev.config.js
@@ -0,0 +1,19 @@
+const config = require('./webpack.config')
+const webpack = require('webpack')
+config.mode = 'development'
+config.devServer = {
+	contentBase: './app',
+	port: 8777,
+	openPage: '',
+	proxy: {
+		"/api/*": {
+			target: "http://localhost:8777/",
+			secure: false,
+			pathRewrite: { "/api": "/" }
+		}
+	}
+}
+
+config.plugins.push(new webpack.HotModuleReplacementPlugin())
+
+module.exports = config
